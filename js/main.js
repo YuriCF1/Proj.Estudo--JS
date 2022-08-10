cfcf2df828331c7221f6ad7302f087b21e869f0a
Yuri <yurifdev@gmail.com>
2022-08-10 00:25:07 -0300

    Adicionando css conforme dados invalidos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
+function mudaCor(classe) {
+    paciente.classList.add(classe);
+}
+
+// OBS: classList me torna em array as classes do eleemento pego pelo querySelector
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i= 0; i < pacientes.length; i++ ) {
+
+    var paciente = pacientes[i]
 
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
@@ -18,6 +27,7 @@ if (peso <= 0 || peso >= 1000) {
     // console.log('Peso inválido');
     tdImc.textContent = 'Peso inválido';
     pesoEhValido = false;
+    mudaCor("paciente-invalido");
 
 }
 
@@ -30,11 +40,19 @@ if (altura <= 0 || altura >= 3.00) {
 
 if (pesoEhValido && alturaEhValida) {
     var imc = peso / (altura * altura)
-    tdImc.textContent = imc;
-} else {
+    // toFixed = definir casas decimais
+    tdImc.textContent = imc.toFixed(2);
+} else if (!pesoEhValido && !alturaEhValida) {
     tdImc.textContent = 'Altura e peso inválidos'
 
 }
+
+}
+
+
+
+
+
 
 // console.log(paciente);
 // console.log(tdPeso);
