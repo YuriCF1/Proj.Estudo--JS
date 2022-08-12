88b0d5a1a3831958390c0806f5ea7bf41f111f1f
Yuri <yurifdev@gmail.com>
2022-08-12 00:11:31 -0300

    Adicionando paciente novo na tabela atrás do form

--- js/main.js
+++ js/main.js
@@ -54,8 +54,37 @@ if (pesoEhValido && alturaEhValida) {
 // OBS: Campos com entrada para usuário possuem o método value e outros ficam com o textContent
 
 var botoaAdicionar = document.querySelector("#adicionar-paciente");
+// Eu poderia usar também o 'onclick', porém o lado ruim dele é que se eu quiser que outra função seja executada, ela vai sobrescrever a anterior
 botoaAdicionar.addEventListener("click", function(event) {
     // Previnir comportamento padrão
     event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+    
+    // Essas propriedades foram pegas do atributo HTML "name"
+    var nomeDado = form.nome.value;
+    var pesoDado = form.peso.value;
+    var alturaDada = form.altura.value;
+    var gorduraDada = form.gordura.value;
+
+    var pacienteNovo = document.createElement("tr");
+    var nomeNovo = document.createElement("td");
+    var pesoNovo = document.createElement("td");
+    var alturaNova = document.createElement("td");
+    var gorduraNova = document.createElement("td");
+
+    nomeNovo.textContent = nomeDado;
+    pesoNovo.textContent = pesoDado;
+    alturaNova.textContent = alturaDada;
+    gorduraNova.textContent = gorduraDada;
+
+    pacienteNovo.appendChild(nomeNovo)
+    pacienteNovo.appendChild(pesoNovo)
+    pacienteNovo.appendChild(alturaNova)
+    pacienteNovo.appendChild(gorduraNova)
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteNovo)
 
 })
